ae3c0b5c21c92b08e74a9511e2b37164151f7ac0
ifpCD <mr.imforpeace.xx@gmail.com>
2026-07-08 17:45:54 -0600

    Cached values in a separate partial

---
+++ Scripts/Runtime/Audio Sources/WyrmBaseSource.Cache.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+
+public partial class WyrmBaseSource : MonoBehaviour, IWyrmSource
+{
+    private AudioClip _clip;
+    private bool _loop;
+    private float _volume;
+    private float _pitch;
+    private float _minDistance;
+    private float _maxDistance;
+
+    public AudioClip clip
+    {
+        get => _clip;
+        set
+        {
+            if (_clip == value) return;
+
+            _clip = value;
+            ASource.clip = value;
+        }
+    }
+
+
+    public bool loop
+    {
+        get => _loop;
+        set
+        {
+            if (_loop == value) return;
+
+            _loop = value;
+            ASource.loop = value;
+        }
+    }
+
+    public float volume
+    {
+        get => _volume;
+        set
+        {
+            if (_volume == value) return;
+
+            _volume = value;
+            ASource.volume = value;
+        }
+    }
+
+    public float pitch
+    {
+        get => _pitch;
+        set
+        {
+            if (_pitch == value) return;
+
+            _pitch = value;
+            ASource.pitch = value;
+        }
+    }
+
+    public float minDistance
+    {
+        get => _minDistance;
+        set
+        {
+            if (_minDistance == value) return;
+
+            _minDistance = value;
+            ASource.minDistance = value;
+        }
+    }
+
+    public float maxDistance
+    {
+        get => _maxDistance;
+        set
+        {
+            if (_maxDistance == value) return;
+
+            _maxDistance = value;
+            ASource.maxDistance = value;
+        }
+    }
+}
