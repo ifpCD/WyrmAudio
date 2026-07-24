d0868a897a2403f06cd7a3bd98cc953ff0927ce7
ifpCD <mr.imforpeace.xx@gmail.com>
2026-07-24 02:52:45 -0600

    Removal of AmbiProcessor

--- Scripts/Runtime/Utility/AmbiProcessor/Examples/AmbiNode.Lifecycle.cs
+++ Scripts/Runtime/Utility/AmbiProcessor/Examples/AmbiNode.Lifecycle.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.Collections;
 using UnityEngine;
 
@@ -5,27 +6,86 @@ public partial class AmbiNode : MonoBehaviour
 {
     internal const int MAXIMUM_CAPACITY = 2000;
 
+    static readonly AmbiNode[] EnabledNodes = new AmbiNode[MAXIMUM_CAPACITY];
+
+    static int _enabledNodeCount;
+
     internal int _index = -1;
 
     void OnEnable()
     {
-        AmbiProcessor.InstanceOnEnableNotification(this);
+        Register(this);
         AmbiOnEnable();
     }
 
     void OnDisable()
     {
-        AmbiProcessor.InstanceOnDisableNotification(this);
+        Deregister(this);
         AmbiOnDisable();
     }
 
     void OnDestroy()
     {
-        AmbiProcessor.InstanceOnDestroyNotification(this);
+        Deregister(this);
         AmbiOnDestroy();
     }
 
-    internal static void Allocate()
+    void Update()
+    {
+        if (_index != 0)
+            return;
+
+        for (var i = 0; i < _enabledNodeCount; i++)
+            EnabledNodes[i].PreBatchUpdate();
+
+        BatchUpdate();
+
+        for (var i = 0; i < _enabledNodeCount; i++)
+            EnabledNodes[i].PostBatchUpdate();
+    }
+
+    static void Register(AmbiNode instance)
+    {
+        if (instance._index >= 0)
+            return;
+
+        if (_enabledNodeCount == MAXIMUM_CAPACITY)
+            throw new InvalidOperationException($"Enabled AmbiNode capacity of {MAXIMUM_CAPACITY} was exceeded.");
+
+        if (_enabledNodeCount == 0)
+            Allocate();
+
+        instance._index = _enabledNodeCount;
+        EnabledNodes[_enabledNodeCount++] = instance;
+        instance.LoadManagedToNative();
+    }
+
+    static void Deregister(AmbiNode instance)
+    {
+        var removedIndex = instance._index;
+
+        if (removedIndex < 0)
+            return;
+
+        var lastIndex = --_enabledNodeCount;
+
+        if (removedIndex != lastIndex)
+        {
+            var movedInstance = EnabledNodes[lastIndex];
+
+            EnabledNodes[removedIndex] = movedInstance;
+            movedInstance._index = removedIndex;
+            SwapBackOnDisable(removedIndex, lastIndex);
+        }
+
+        EnabledNodes[lastIndex] = null;
+        instance._index = -1;
+
+        if (_enabledNodeCount == 0)
+            Deallocate();
+    }
+
+    static void Allocate()
     {
         NodeExtents = new(length: MAXIMUM_CAPACITY, allocator: Allocator.Persistent);
         ListenerRoomID = new(allocator: Allocator.Persistent);
@@ -33,7 +93,7 @@ public partial class AmbiNode : MonoBehaviour
         Transforms = new(capacity: MAXIMUM_CAPACITY);
     }
 
-    internal static void Deallocate()
+    static void Deallocate()
     {
         if (NodeExtents.IsCreated)
             NodeExtents.Dispose();
@@ -45,7 +105,7 @@ public partial class AmbiNode : MonoBehaviour
             Transforms.Dispose();
     }
 
-    internal static void SwapBackOnDisable(int removedIndex, int lastIndex)
+    static void SwapBackOnDisable(int removedIndex, int lastIndex)
     {
         NodeExtents[removedIndex] = NodeExtents[lastIndex];
         Transforms[removedIndex] = Transforms[lastIndex];
